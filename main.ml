91af8da934786269acdff3bf9350bbb6c8764fb0
Shihao Cao <shehowcow@gmail.com>
2020-04-23 00:05:06 -0400

    Added Make Play

--- main.ml
+++ main.ml
@@ -1,8 +1,11 @@
 let main () =
   ANSITerminal.(print_string [red]
-                  "\n\nWelcome to the 3110 Text Adventure Game engine.\n");
+                  "\n\nWelcome to the Tank Game engine.\n");
   print_endline "Please enter the name of the game file you want to load.\n";
-  print_string  "> ";
+  print_string  "> "
   (* match read_line () with
   | exception End_of_file -> ()
   | file_name -> play_game file_name *)
+
+(* Execute the game engine. *)
+let () = main ()
