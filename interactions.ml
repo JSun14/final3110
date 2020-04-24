d490197023ec251bd738f9bd07e68efbc95b78d9
Jerry Sun <js2746@cornell.edu>
2020-04-23 20:43:13 -0700

    Finish skeleton implementations of all interaction functions

--- interactions.ml
+++ interactions.ml
@@ -38,24 +38,42 @@ let get_distance_from pointA pointB =
        (snd(pointA) -. snd(pointB))*.(snd(pointA) -. snd(pointB)))
 
 (**[hitbox_detect tank projs] takes in a tank and tests if any projectiles are
-   in the tank's hitbox and returns a boolean of whether the tank was hit or not *)
+   in the tank's hitbox and returns true if a projectile hit a tank and false
+   if it didn't*)
 let rec hitbox_detect tank projs= 
   match projs with
   | [] -> false
-  | h::t -> if get_distance_from (get_pos h) (get_pos tank) < 1.0 then true else
-      false
+  | h::t -> if get_distance_from (get_pos h) (get_pos tank) < 1.0 then true 
+    else hitbox_detect tank t
 
 (**[tank removal projs tanks] takes in a list of tanks and projectiles and 
    returns back the list of active tanks and removes tanks hit by projectiles*)
-let tank_removal projs tanks= 
-  failwith ("Unimplemented")
+let rec tank_removal projs tanks= 
+  match tanks with
+  | [] -> []
+  | h::t -> if hitbox_detect h projs then h:: tank_removal projs t 
+    else tank_removal projs t 
 
-(**[wall_detect proj walls] takes in a proj and a list of walls and returns back
-   a boolean of whether the project has hit a wall *)
-let wall_detect proj walls= 
-  failwith ("Unimplemented")
+(**[wall_detect proj walls] takes in a projectile's coordinates as a tuple and a
+   list of walls and returns back a boolean of whether the project hit a wall*)
+let rec wall_detect proj walls= 
+  match walls with
+  | [] -> false
+  | h::t -> let x = get_pos h in 
+    if (fst x) <= (fst proj) && (fst x +. 1.0) >= (fst proj) 
+       && (snd x) <= (snd proj) && (snd x +. 1.0) >= (snd proj) then true 
+    else wall_detect proj t
 
-(**[proj_removal projs tanks obs] takes in a list of projs, tanks, and obs and
+let rec tank_detect tanks proj=
+  match tanks with
+  | [] -> false
+  | h::t -> if get_distance_from (get_pos proj) (get_post h) < 1.0 then true
+    else tank_detect t proj
+
+(**[proj_removal projs tanks obs] takes in a list of projs, tanks, and walls and
    returns back an active list of projectiles *)
-let proj_removal projs tanks obs= 
-  failwith ("Unimplemented")
+let rec proj_removal projs tanks walls= 
+  match projs with
+  | [] -> []
+  | h::t -> if tank_detect tanks h && wall_detect h walls 
+    then h::proj_removal t tanks walls else proj_removal t tanks walls
