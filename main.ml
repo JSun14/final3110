e062aecda0834c2f778b759e465b9cc6ca87e878
Kaitlyn Chen <kjc256@cornell.edu>
2020-04-26 03:09:51 -0400

    fixed json issues

--- main.ml
+++ main.ml
@@ -21,8 +21,9 @@ let init_world map = {
 }
 
 let rec game_helper w st =
-(* print debug info about game state *)
+  (* print debug info about game state *)
   print_state st;
+  State.print_tank_info st;
   Render.clear;
 
   Unix.sleepf(0.01);
@@ -31,7 +32,7 @@ let rec game_helper w st =
   let _ = print_user_in u_in in
   let s2 = Process.process_u_in st u_in in
   let s3 = Interactions.execute w s2 in
-(* Render.execute world s3 *)
+  (* Render.execute world s3 *)
 
   let final_state = {
     s3 with cycle_no = st.cycle_no + 1
@@ -43,11 +44,11 @@ let rec game_helper w st =
   else
     game_helper w final_state
 
-  (* sleep for 1/100 of second *)
+(* sleep for 1/100 of second *)
 
 let json_file_to_map f =
   let json = f |> Yojson.Basic.from_file in
-    json |> Read_json.from_json
+  json |> Read_json.from_json
 
 let main () =
   ANSITerminal.(print_string [red]
@@ -55,8 +56,8 @@ let main () =
   print_endline "Please enter the name of the game file you want to load.\n";
   print_string  "> ";
   (* match read_line () with
-  | exception End_of_file -> ()
-  | file_name -> play_game file_name *)
+     | exception End_of_file -> ()
+     | file_name -> play_game file_name *)
   start_rend;
 
   let w = init_world (json_file_to_map "map0.json") in
