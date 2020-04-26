23bfb29a70a248b7b008c0fabbb2a9aea44c5e35
Kaitlyn Chen <kjc256@cornell.edu>
2020-04-26 01:09:29 -0400

    Added win/loss conditions to state.ml

--- state.ml
+++ state.ml
@@ -22,10 +22,37 @@ type world = {
   ditch_list : Block.block list;
 }
 
+(** [is_player_dead st] is true when the player tank is dead and false
+    when the player tank is alive. 
+
+    Requires: [st] is a valid game state *)
 let is_player_dead st = 
+  (** [helper lst] is true when the player tank is dead and false
+      when the player tank is alive. *)
   let rec helper lst = match lst with 
     | [] -> true
     | h :: t -> 
       if h.side = Enemy then helper t
       else false
   in helper st.tanks
+
+(** [loss_condition st] is whether the player has lost the game. 
+    [loss_condition st] is true when the player has lost, and false if
+    the game is ongoing or the player has won. *)
+let loss_condition st =
+  is_player_dead st
+
+(** [win_condition st] is whether the player has won the game. 
+    It is true when the player has won the game and false if it is
+    ongoing or the player has lost. 
+
+    Requires: [st] is a valid game state. *)
+let win_condition st = 
+  (** [all_enemies_dead lst] is true when all the enemy tanks are inactive
+      and false if at least one enemy tank still is active. *)
+  let rec all_enemies_dead lst = match lst with
+    | [] -> false
+    | h :: t -> 
+      if h.side = Self then all_enemies_dead t
+      else false
+  in all_enemies_dead st.tanks
