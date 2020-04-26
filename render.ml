b6a953b4799875bd500c3103cac7412d6a6cbd85
Shihao Cao <shehowcow@gmail.com>
2020-04-26 04:30:51 -0400

    more griddage

--- render.ml
+++ render.ml
@@ -68,7 +68,8 @@ let draw_grid () =
 
   let dummy = [0.0; 1.0; 2.0; 3.0; 4.0; 5.0; 6.0; 7.0; 8.0; 9.0; 10.0; 
     11.0; 12.0; 13.0; 14.0; 15.0; 16.0; 17.0; 18.0; 19.0; 20.0] in 
-  let d_scaled = List.map (fun x -> x *. scale |> int_of_float) dummy in 
+  let d2 = dummy @ [21.0; 22.0; 23.0; 24.0; 25.0; 26.0; 27.0; 28.0; 29.0] in
+  let d_scaled = List.map (fun x -> x *. scale |> int_of_float) d2 in 
 
   let draw_horiz y_val = 
     moveto 0 y_val;
