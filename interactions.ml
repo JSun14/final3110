ee9ba31e9b432f79d15a929fd27fc7b7307b585d
Jerry Sun <js2746@cornell.edu>
2020-05-03 20:15:11 -0700

    Tank + Projectile removal

--- interactions.ml
+++ interactions.ml
@@ -16,14 +16,6 @@ let rec move_tank (tanks:Movable.tank list) walls=
      health = h.health; last_fire_time = h.last_fire_time; 
      side = h.side;}::move_tank t walls
 
-(*match wall_detect new_loc walls with
-  | Some w -> {loc = h.loc; past_loc = h.loc; velocity = (0.0,0.0); 
-             health = h.health; last_fire_time = h.last_fire_time; 
-             side = h.side;}::move_tank t walls
-  | None -> {loc = new_loc; past_loc = h.loc; velocity = (0.0,0.0); 
-           health = h.health; last_fire_time = h.last_fire_time; 
-           side = h.side;}::move_tank t walls*)
-
 
 (**[move_projs projs walls] is a list of active projectiles with updated 
    locations. If the proj's new location is inside a wall then remove it, but if 
@@ -37,16 +29,6 @@ let rec move_projs (projs:Movable.projectile list) walls=
      health = h.health; weap_species= h.weap_species;}
     ::move_projs t walls
 
-(*match wall_detect new_loc walls with
-  | None -> {loc = new_loc; past_loc = h.loc; velocity = (0.0,0.0); 
-           health = h.health; weap_species= h.weap_species;}
-          ::move_projs t walls
-  | Some w -> match h.weap_species with
-  | Bouncy -> if h.health = 2 
-    then {loc = h.loc; past_loc = h.loc; velocity = (0.0,0.0); 
-          health = h.health; weap_species= h.weap_species;}::move_projs t walls
-    else move_projs t walls
-  | Standard -> move_projs t walls*)
 
 (**[wall_detect coords walls] takes in a entity's coordinates as a tuple and a
    list of walls and returns back a boolean of whether the entity is in a wall*)
@@ -131,7 +113,7 @@ let rec check_proj_wall projs walls=
 (**[hitbox_detect tank projs] takes in a tank and tests if any projectiles are
    in the tank's hitbox and returns true if a projectile hit a tank and false
    if it didn't*)
-let rec hitbox_detect tank projs= 
+let rec hitbox_detect (tank:Movable.tank) (projs:Movable.projectile list)= 
   match projs with
   | [] -> false
   | h::t -> if get_distance_from h.loc tank.loc < 0.4 then true 
@@ -139,7 +121,7 @@ let rec hitbox_detect tank projs=
 
 (**[tank removal projs tanks] takes in a list of tanks and projectiles and 
    returns back the list of active tanks and removes tanks hit by projectiles*)
-let rec tank_removal projs tanks= 
+let rec tank_removal (projs:Movable.projectile list) (tanks:Movable.tank list) : Movable.tank list= 
   match tanks with
   | [] -> []
   | h::t -> if hitbox_detect h projs then h:: tank_removal projs t 
@@ -147,7 +129,7 @@ let rec tank_removal projs tanks=
 
 (**[tank_detect tanks proj] takes in a list of tanks and a projectile and checks
    to see if a projectile should be removed due to it hitting a tank via bool *)
-let rec tank_detect tanks proj=
+let rec tank_detect (tanks: Movable.tank list) proj=
   match tanks with
   | [] -> false
   | h::t -> if get_distance_from proj.loc h.loc < 0.4 then true
@@ -155,12 +137,14 @@ let rec tank_detect tanks proj=
 
 (**[proj_removal projs tanks obs] takes in a list of projs, tanks, and walls and
    returns back an active list of projectiles *)
-let rec proj_removal projs tanks walls= 
+let rec proj_removal (projs:Movable.projectile list) (tanks:Movable.tank list) walls : Movable.projectile list = 
   match projs with
   | [] -> []
   | h::t -> if tank_detect tanks h  
     then h::proj_removal t tanks walls else proj_removal t tanks walls
 
+let entity_removal_execute w (st:State.state)=
+  {st with tanks=tank_removal st.projectiles st.tanks ; projectiles=proj_removal st.projectiles st.tanks w.wall_list}
 let wall_execute w (st:State.state)=
   {st with tanks= check_tank_wall st.tanks (w.wall_list@w.ditch_list);}
 let execute w (st:State.state)= 
