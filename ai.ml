4ea06cbd8547cddee062a07c4169d3ad0acf2a05
Kaitlyn Chen <kjc256@cornell.edu>
2020-05-07 19:38:32 -0400

    randomized enemy tank velocity

--- ai.ml
+++ ai.ml
@@ -4,14 +4,18 @@ open State
 open Util
 open Const
 
+(** [random_mag] is either 1.0 or -1.0 with equal probability. *)
 let random_mag =
   if Random.bool () then 1.0
   else -1.0
 
+(** [cap_velocity t accel] returns true when the sum of [accel] and the 
+    velocity of [t] is less than or equal to an enemy's maximum speed. *)
 let cap_velocity (t : Movable.tank) accel = 
   fsum t.velocity accel |> magn <= max_enemy_speed
 
-(* take a velocity and add a random velocity (mag and direction) *)
+(* [randomize t] is a tank with a new, random acceleration added to 
+   [t]'s velocity. *)
 let rec randomize (t : Movable.tank) : Movable.tank =
   let x_accel = Random.float Const.player_speed in
   let y_accel = Random.float Const.player_speed in
@@ -19,8 +23,11 @@ let rec randomize (t : Movable.tank) : Movable.tank =
   if cap_velocity t accel then {t with velocity = fsum t.velocity accel}
   else randomize t
 
-let move_enemy_tank (t : Movable.tank) : Movable.tank =
-  {t with loc = (fst t.loc +. fst t.velocity, snd t.loc +. snd t.velocity)}
+(** [move_all_enemies st] is a state where all enemy tanks in [st] have
+    a randomized velocity.
 
-let move_all_enemies lst = 
-  List.map (fun x -> move_enemy_tank (randomize x)) lst
+    Requires: [st] is a valid game state.  *)
+let move_all_enemies st = 
+  let updated_tanks = 
+    List.map (fun x -> if x.side = Enemy then randomize x else x) st.tanks in
+  {st with tanks = updated_tanks}
