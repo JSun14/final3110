72931d473ed476be8811a331389e76f557c5766b
Jerry Sun <js2746@cornell.edu>
2020-05-09 18:03:28 -0700

    Finish Documentation

--- block.ml
+++ block.ml
@@ -1,9 +1,11 @@
+(**type block_type is a variant of wall and ditch*)
 type block_type = Wall | Ditch
 
+(**type block is a record of a block's id, kind, width, and coordinates *)
 type block = {
-    id : string;
-    kind : block_type;
-    width : float;
-    (* Walls should be centrally located at multiples of n + 0.5 *)
-    coord : float * float;
+  id : string;
+  kind : block_type;
+  width : float;
+  (* Walls should be centrally located at multiples of n + 0.5 *)
+  coord : float * float;
 }
